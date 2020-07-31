8bf9f28ce6a17bd1d0a50ccf0ce5fa7798d6f3bf
Nitesh <njhawar01@gmail.com>
2020-07-31 22:21:38 +0530

    Logging using log4net added

--- Controllers/PackagingAndDeliveryController.cs
+++ Controllers/PackagingAndDeliveryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PackagingAndDelivery.Models;
 
 namespace PackagingAndDelivery.Controllers
 {
@@ -11,11 +12,13 @@ namespace PackagingAndDelivery.Controllers
     [ApiController]
     public class PackagingAndDeliveryController : ControllerBase
     {
+        static readonly log4net.ILog _log4net = log4net.LogManager.GetLogger(typeof(PackagingAndDeliveryController));
         [HttpGet]
         public dynamic GetPackagingDeliveryCharge(string item, int count)
         {
+            _log4net.Info("GetPackagingDeliveryCharge() called");
             int charges = 0;
-            if (item == null || count < 0)
+            if (item == null || count <= 0)
             {
                 return BadRequest("Input not valid");
             }
