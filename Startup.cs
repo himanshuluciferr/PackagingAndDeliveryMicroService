be93696ed190dc3d50413b9c59269b9fcdd8f76b
Nitesh <njhawar01@gmail.com>
2020-07-31 14:24:27 +0530

    Swagger implemented, Weatherforecast removed

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace PackagingAndDelivery
 {
@@ -26,6 +27,10 @@ namespace PackagingAndDelivery
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Packaging & Delivery", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +40,13 @@ namespace PackagingAndDelivery
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "Packaging & Delivery V1");
+            });
 
             app.UseHttpsRedirection();
 
