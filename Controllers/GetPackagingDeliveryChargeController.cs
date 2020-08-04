bde224f57756532275ad73f6f6df5dce90986e1a
Nitesh <njhawar01@gmail.com>
2020-08-04 19:42:51 +0530

    Added Interface for controller

--- Controllers/GetPackagingDeliveryChargeController.cs
+++ Controllers/GetPackagingDeliveryChargeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PackagingAndDelivery.Models;
 using LINQtoCSV;
+using PackagingAndDelivery.Repository;
 
 namespace PackagingAndDelivery.Controllers
 {
@@ -14,36 +15,28 @@ namespace PackagingAndDelivery.Controllers
     [ApiController]
     public class GetPackagingDeliveryChargeController : ControllerBase
     {
+        public ICharges _context;
+        public GetPackagingDeliveryChargeController(ICharges context)
+        {
+            this._context = context;
+        }
         static readonly log4net.ILog _log4net = log4net.LogManager.GetLogger(typeof(GetPackagingDeliveryChargeController));
         [HttpGet]
         [ActionName("GetPackagingDeliveryCharge")]
+
         public dynamic GetPackagingDeliveryCharge(string item, int count)
         {
             _log4net.Info("GetPackagingDeliveryCharge() called");
-            if(count <= 0)
+            dynamic charges = _context.GetPackagingDeliveryCharge(item, count);
+            if (charges == -1)
             {
-                return BadRequest("Count not valid");
+                return BadRequest("Count not valid"); 
             }
-            var CSVFile = new CsvFileDescription
+            else
             {
-                SeparatorChar = ',',
-                FirstLineHasColumnNames = true
-            };
-            int Charge = 0;
-            var CSV = new CsvContext();
-            var Charges = from values in CSV.Read<Item>(@"./Items.csv", CSVFile)
-                          where (values.ItemType.Trim().ToUpper() == item.ToUpper())
-                          select new
-                          {
-                              DeliveryCharge = values.Delivery,
-                              PackagingCharge = values.Packaging
-                          };
-            var Fee = Charges.Select(charge => charge.DeliveryCharge + charge.PackagingCharge).ToList();
-            foreach(int value in Fee)
-            {
-                Charge += value;
+                return charges;
             }
-            return Charge * count;
         }
+        
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using PackagingAndDelivery.Repository;
 
 namespace PackagingAndDelivery
 {
@@ -27,10 +28,13 @@ namespace PackagingAndDelivery
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Packaging & Delivery", Version = "v1" });
             });
+
+            services.AddScoped<ICharges, Charges>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
